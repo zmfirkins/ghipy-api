353649fe7d141eebfc575a819a462620d2cffcf6
Zoie Firkins <zmfirkins@dmacc.edu>
2025-10-02 08:30:38 -0500

    Setting things up

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log("script.js loaded");
+// Giphy API key
+const API_KEY = "s2SYHnCQ2JKcLIz5QzgmlsFO0ZWFQ6Ar"
+
+// Grab elements
+const fetchBtn = document.getElementById("fetch-gif-btn")
+const gifContainer = document.getElementById("gif-container")
+const searchInput = document.getElementById("search-input")
